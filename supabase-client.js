4ee22a28dade6065900c6ed8307495bb50940249
ramennano <ramennano@gmail.com>
2026-09-25 01:06:25 +0700

    Delete supabase-client.js

--- supabase-client.js
+++
@@ -1,5 +0,0 @@
-// Ganti URL dan ANON_KEY sesuai dengan project Supabase Anda
-const SUPABASE_URL = "https://gyortxfcoifxzrwfogzr.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd5b3J0eGZjb2lmeHpyd2ZvZ3pyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAxNzE2NzEsImV4cCI6MjEwNTc0NzY3MX0.dO07GyxM9WYRMHYOE70-nJQI_TONr7SXK7loXLRdkHU";
-
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
